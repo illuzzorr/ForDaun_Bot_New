f50939d3272b5604ac3344557fb596526926c96e
illuzzorr <daniilbuzko89@gmail.com>
2025-03-27 21:31:40 +0300

    with me

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def reply_to_target_user(message: types.Message):
         await message.reply(response)
 
     if message.from_user.id == TARGET_USER_ID2:
-        await message.reply("йоу")
+        await message.reply("йоууу")
 
 async def main():
     logging.basicConfig(level=logging.INFO)
